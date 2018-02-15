ab962e5324b6b01ff6a1dd458c097e50df9ef406
Steve Adams <steve@threestraightlines.co.uk>
2018-02-15 18:26:06 +0000

    Update "standard" framework exceptions to use new abstract

--- src/Exceptions/Curl.php
+++ src/Exceptions/Curl.php
@@ -1,17 +1,9 @@
 <?php
 	namespace Bolt\Exceptions;
 
-	use \Exception;
+	use Bolt\Exception;
 
 	class Curl extends Exception
 	{
-		protected $codes;
-
-		public function __construct($code, Exception $previous = null)
-		{
-			$codes = new Codes\Curl();
-
-			parent::__construct($codes->fromCode($code), $code, $previous);
-		}
 	}
 ?>
