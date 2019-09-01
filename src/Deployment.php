928c69f3e55644ec8534ffe66d04330638544dd4
Steve Adams <irwtdvoys@gmail.com>
2019-09-01 12:03:07 +0100

    Add Deployment enum

---
+++ src/Deployment.php
@@ -0,0 +1,11 @@
+<?php
+	namespace Bolt;
+
+	class Deployment extends Enum
+	{
+		const PRODUCTION = "production";
+		const STAGING = "staging";
+		const TESTING = "testing";
+		const DEVELOPMENT = "development";
+	}
+?>
