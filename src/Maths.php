0480278aa8717b6de010001b99a07625b06413e9
Steve Adams <irwtdvoys@gmail.com>
2020-01-22 12:10:14 +0000

    Add GCD and LCM methods

--- src/Maths.php
+++ src/Maths.php
@@ -87,5 +87,34 @@
 		{
 			return 2 * pi();
 		}
+
+		/**
+		 * Greatest Common Divisor
+		 *
+		 * @param int $a
+		 * @param int $b
+		 * @return int
+		 */
+		public static function gcd(int $a, int $b): int
+		{
+			if ($a == 0)
+			{
+				return $b;
+			}
+
+			return self::gcd($b % $a, $a);
+		}
+
+		/**
+		 * Lowest Common Multiple
+		 *
+		 * @param int $a
+		 * @param int $b
+		 * @return int
+		 */
+		public static function lcm(int $a, int $b): int
+		{
+			return ($a * $b) / self::gcd($a, $b);
+		}
 	}
 ?>
